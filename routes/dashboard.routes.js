dc80feb6fb0e3c4838b30a6a3f1f7f03484aaedd
Matt Struhs <mattstruhs@yahoo.com>
2021-06-29 22:08:45 -0400

    redirect when no user

--- routes/dashboard.routes.js
+++ routes/dashboard.routes.js
@@ -1,9 +1,7 @@
 const router = require("express").Router();
-// const { response } = require("express");
-// const Celebrity = require("../models/Celebrity.model");
+const isLoggedIn = require("../middleware/isLoggedIn");
 
-/* GET home page */
-router.get("/", async (req, res, next) => {
+router.get("/", isLoggedIn, async (req, res, next) => {
   const testString = "this is the dashboard route";
   res
     .status(200)
@@ -13,35 +11,5 @@ router.get("/", async (req, res, next) => {
     });
 });
 
-// router.get("/:id", async (req, res, next) => {
-//   const celebrity = await Celebrity.findById(req.params.id);
-//   res.status(200).json({ celebrity });
-// });
-
-// router.post("/create", async (req, res, next) => {
-//   Celebrity.create(req.body).then((createdCeleb) => {
-//     console.log(createdCeleb);
-//     res.json({ createdCeleb });
-//   });
-// });
-
-// // creating Celebrity separately from uploading img to Cloudinary
-// router.post("/celebrity", async (req, res, next) => {
-//   const newCeleb = await Celebrity.create(req.body);
-//   res.json({ newCeleb });
-// });
-
-// router.post("/:id/edit", (req, res, next) => {
-//   const id = req.params.id;
-//   const { name, occupation, catchphrase } = req.body;
-//   Celebrity.findByIdAndUpdate(
-//     id,
-//     { name, occupation, catchphrase },
-//     { new: true }
-//   ).then((response) => {
-//     console.log("response", response);
-//     res.json({ celebrity: response });
-//   });
-// });
 
 module.exports = router;
